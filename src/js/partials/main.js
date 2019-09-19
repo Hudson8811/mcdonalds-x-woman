f0fe72f75671e0e6cd48a625f5e11eb521ec67a1
db21 <you@example.com>
2019-09-19 15:02:44 +0300

    header

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
 
+    //header
+    var burger = $('.burger');
+    var hMenu = $('header .menu');
 
+    burger.on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        hMenu.stop().slideToggle(200);
+    });
+    $(window).resize(function () {
+        if (!burger.is(':visible')) {
+            hMenu.attr('style', '');
+        }
+    });
 });

b7836ed6bfa05cdd81558b636b21b315466c57ad
db21 <you@example.com>
2019-09-19 19:47:19 +0300

    section2-50%

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@
 //= libs/jquery-3.3.1.min.js
 //= libs/jquery.fancybox.min.js
 //= libs/jquery.maskedinput.min.js
-
+//= libs/isInViewport.min.js
+//= libs/jquery.scroolly.min.js
 /* my scripts */
 
 //= partials/main.js
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -16,4 +16,38 @@ $(document).ready(function () {
             hMenu.attr('style', '');
         }
     });
+    //end header
+
+
+
+
+    //Как было анимация первого блока
+    $('.section-2 .year-1 .soldiers').scroolly([
+        {
+            from: 'el-top = vp-bottom',
+            to: 'el-bottom = vp-top',
+            cssFrom: { width: '340px' },
+            cssTo: { width: '110px' }
+        },
+        {
+            from: 'el-top = vp-bottom',
+            to: 'el-bottom = vp-top',
+            cssFrom: { left: '0px' },
+            cssTo: { left: '115px' }
+        }
+    ]);
+
+    var carAnim=true;//анимация одноразовая. поэтому делаем проверку
+    if(carAnim && $('.section-2 .year-1 .car').isInViewport({ tolerance: -100})){
+        $('.section-2 .year-1 .car').addClass('animation-car-desctop');
+        carAnim=false;
+
+    }
+
+    $(window).scroll(function () {
+        if( carAnim && $('.section-2 .year-1 .car').isInViewport({ tolerance: -100})){
+            $('.section-2 .year-1 .car').addClass('animation-car-desctop');
+            carAnim=false;
+        }
+    });
 });
